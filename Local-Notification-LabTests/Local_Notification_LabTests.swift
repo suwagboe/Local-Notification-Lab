1407159ae18fd5f4ea6f3b43422ae4b4a9514e6e
uwagboe <sm.uwagboe@gmail.com>
2020-02-20 14:23:27 -0500

    Initial Commit

---
+++ Local-Notification-LabTests/Local_Notification_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Local_Notification_LabTests.swift
+//  Local-Notification-LabTests
+//
+//  Created by Pursuit on 2/20/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import XCTest
+@testable import Local_Notification_Lab
+
+class Local_Notification_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
